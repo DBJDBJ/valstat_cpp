2e92daacef79cefc818d3ebd4115a6278a8e49a6
dbj <dbj@dbj.org>
2021-03-14 11:38:20 +0100

    large-ish update

---
+++ various_prose/sample/dbj_strerror.h
@@ -0,0 +1,125 @@
+#ifndef DBJ_STERROR_INC_
+#define DBJ_STERROR_INC_
+/*
+First problem is this page: https://docs.microsoft.com/en-us/cpp/c-language/strerror-function?view=msvc-160
+It is unclear if that applies since there is also:
+
+const char * const msg_ = _sys_errlist[ec_] ;
+
+MSVC crt error messages are in  _sys_errlist[0 .. 43]
+errno outside of that message is "Unknown Error"
+which is _sys_errlist[43]
+
+For legal errno see:
+https://docs.microsoft.com/en-us/cpp/c-runtime-library/errno-doserrno-sys-errlist-and-sys-nerr?view=msvc-160
+*/
+
+/*
+Bounds-checked functions, 
+are only guaranteed to be available if 
+__STDC_LIB_EXT1__ is defined by the implementation 
+and if the user defines __STDC_WANT_LIB_EXT1__ to the integer constant 1
+before including
+*/
+#define __STDC_LIB_EXT1__
+#define __STDC_WANT_LIB_EXT1__ 1
+
+/*
+As all functions from Dynamic Memory TR, strndup is only guaranteed
+to be available if __STDC_ALLOC_LIB__ is defined by the implementation
+and if the user defines __STDC_WANT_LIB_EXT2__ to the integer 
+constant 1 before including string.h.
+*/
+
+#ifdef __STDC_ALLOC_LIB__
+#define __STDC_WANT_LIB_EXT2__ 1
+#else
+#define _POSIX_C_SOURCE 200809L
+#endif
+
+#include <assert.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <errno.h>
+#include <locale.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif // __cplusplus
+    enum
+    {
+        dbj_strerror_max_len = 1024,
+        dbj_strerror_mid_len = 512,
+    };
+    typedef struct
+    {
+        char data[dbj_strerror_max_len];
+    } dbj_strerror_msg;
+
+    /* 
+    ec_ : the errno value 
+    user_msg_ : max len 512 
+    */
+    inline dbj_strerror_msg dbj_strerror(
+        unsigned int ec_, const char user_msg_[dbj_strerror_mid_len])
+    {
+        assert(strnlen_s(user_msg_, dbj_strerror_mid_len) < dbj_strerror_mid_len);
+        /*
+https://docs.microsoft.com/en-us/cpp/c-runtime-library/errno-doserrno-sys-errlist-and-sys-nerr?view=msvc-160
+on WINDOWS there is only a subset of errno messages in use
+And "illegal" errno message is "Unknown Error" which is misleading
+thus we will return: "Not a legal Windows errno: %d"
+NOTE: if Windows SDK changes errno.h this will have to be changed
+chances of that happening are very close to zero. But still not zero.
+date of this code: 2021-03-14
+*/
+#ifdef _WIN32
+        bool illegal_win_errno = false;
+        switch (ec_)
+        {
+        case 15:
+        case 26:
+        case 37:
+            illegal_win_errno = true;
+            break;
+        default:
+        {
+            // over 42 only 80 is legal
+            if ((ec_ > 42) && (ec_ != 80))
+                illegal_win_errno = true;
+        }
+        } // switch
+        if (illegal_win_errno)
+        {
+            dbj_strerror_msg msg_ = {0};
+            snprintf(msg_.data, dbj_strerror_max_len, "Not a legal Windows errno: %d", ec_);
+            return msg_;
+        }
+#endif // _WIN32
+
+        static const char *const fmt_ = "%s (%s)";
+        char errmsg[dbj_strerror_mid_len]{0};
+
+        int rezult = strerror_s(errmsg, dbj_strerror_mid_len, ec_);
+        assert(rezult == 0);
+
+        size_t size_required = 1 +
+                               snprintf(nullptr, 0, fmt_, errmsg, user_msg_);
+
+        assert((size_required > 1) && (size_required < dbj_strerror_max_len));
+
+        dbj_strerror_msg msg_ = {0};
+        snprintf(msg_.data, dbj_strerror_max_len, fmt_, errmsg, user_msg_);
+
+        return msg_;
+    }
+
+#ifdef __cplusplus
+} // extern "C"
+#endif // __cplusplus
+
+#endif // DBJ_STERROR_INC_
--- various_prose/sample/non trivial but simple sample.cpp
+++ various_prose/sample/non trivial but simple sample.cpp
@@ -1,43 +1,22 @@
 /*
 https://godbolt.org/z/7bn9r5
 */
-/*
-Bounds-checked functions, 
-are only guaranteed to be available if 
-__STDC_LIB_EXT1__ is defined by the implementation 
-and if the user defines __STDC_WANT_LIB_EXT1__ to the integer constant 1
-before including
-*/
-#define __STDC_LIB_EXT1__
-#define __STDC_WANT_LIB_EXT1__ 1
 
-/*
-As all functions from Dynamic Memory TR, strndup is only guaranteed
-to be available if __STDC_ALLOC_LIB__ is defined by the implementation
-and if the user defines __STDC_WANT_LIB_EXT2__ to the integer 
-constant 1 before including string.h.
-*/
+#include "dbj_strerror.h"
 
-#ifdef __STDC_ALLOC_LIB__
-#define __STDC_WANT_LIB_EXT2__ 1
-#else
-#define _POSIX_C_SOURCE 200809L
-#endif
-
-#include <assert.h>
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-#include <string.h>
-#include <errno.h>
-#include <locale.h>
+// https://stackoverflow.com/a/33206814
+#define VT_WHITE_BRIGHT "\033[97;1m"
+#define VT_RED_BRIGHT "\033[91;1m"
+#define VT_RESET "\033[0m"
+#define RED_MSG(S_) VT_RED_BRIGHT S_ VT_RESET
+#define WHT_MSG(S_) VT_WHITE_BRIGHT S_ VT_RESET
 
 /*
  (c) 2020 by dbj@dbj.org
  LICENSE_DBJhttps://dbj.org/license_dbj/
  valstat(tm) and metastate(tm) are protected trade marks
 
-std-proposals discussionimplement this with valstat
+2020 Q4, one std-proposals discussion was centered arround this use-case
 
  std::optional<std::string> FindUsersCity() {
    std::optional<ContactsServer> contacts = GetOrOpenContactsServerConnection();
@@ -46,9 +25,14 @@ std-proposals discussionimplement this with valstat
    std::optional<Location>       uloc     = geo.GetOrReturnNullOpt()->GetLocation(*uid);
    return uloc.GetOrReturnNullOpt()->GetCityName();
 }
-   where GetOrReturnNullOpt() is a method that
+   For example GetOrReturnNullOpt() is a method that
    (1) either returns a value if std::optional is not empty,
-   (2) or returns from FindUsersCity() if std::optional is empty.
+   (2) or returns (exits) from FindUsersCity() if std::optional is empty.
+
+   Bary Revzin suggested an very interesting C++20 solution using
+   the co_await machinery
+
+   bellow is my solution based around valstat
 */
 #undef DBJ_CPP03
 #undef DBJ_CPP11
@@ -71,119 +55,87 @@ std-proposals discussionimplement this with valstat
 #error Unsuported C++ version
 #endif
 
-//
-// system_error is legacy + technical debt
-// in reality one can implement her own
-// auto ecs_ =  std::make_error_code( ec_ ).message() ;
-// easily
-//
 #include <optional>
-#include <vector>
- 
-using buff_t = std::vector<char>;
-
-/*
-    const char * const msg_ = _sys_errlist[ec_] ;
-
-MSVC crt error messages are in  _sys_errlist[0 .. 43]
-errno outside of that message is "Unknown Error"
-which is _sys_errlist[43]
-
-For legal errno see:
-https://docs.microsoft.com/en-us/cpp/c-runtime-library/errno-doserrno-sys-errlist-and-sys-nerr?view=msvc-160
-*/
-static std::vector<char> errc_str(int ec_, const char *user_msg_)
-{
-    constexpr static auto fmt_ = "%s (%s)" ;
-    char errmsg[BUFSIZ]{0};
-
-    strerror_s(errmsg, BUFSIZ, ec_);
-
-    size_t size_required = 
-        snprintf(nullptr, 0, fmt_, errmsg, user_msg_);
-    assert(size_required > 1) ;
-
-    std::vector<char> buf(2 + size_required, char('?'));
-        snprintf(buf.data(), buf.size(), fmt_, errmsg, user_msg_);
-
-    return buf ;
-}
 
 static auto initialize_once = []() {
     srand((unsigned)time(0));
     return true;
 }();
 
-// random 0/1 flip
-// srand() is called above
-#define RANDOM_0_OR_1 bool(rand() % 2)
-
-/*
-    // for tight runtimes
-    // this depends on two facts
-    // (value) yields true for value != null
-    // (status) yields true if status != null
-	template<typename T> struct valstat final {
-		T* value;
-        // by using the outcome message
-        // instead of some specific status and its type 
-        // we are completely decoupled from the 
-        // HOW is this message made
-        // for traceability one might mandate
-        // JSON format for status messages
-        // obviously mandate is never to free status messages
- 		const char * status;
-	};
-
-    // one could aid the key metastate attribute
-    // and that is one of emptiness
-    inline bool empty_value = [] ( auto vstat_ ) { return vstat_->value != nullptr ; };
-
-    inline bool empty_status = [] ( auto vstat_ ) { return vstat_->status != nullptr ; };
-
-*/
-/* 
-    THE VALSTAT 
-    half fat version 
-*/
+/* the VALSTAT structure */
 template <typename T>
 struct valstat final
 {
     std::optional<T> value;
     /* 
-status (if not empty) is errno message + user prompt
-*/
-    std::optional< std::vector<char> > status{};
+    status (if not empty) is errno message + user prompt 
+    see dbj_strerror.h for details
+    */
+    std::optional<dbj_strerror_msg> status{};
 };
+/*
+NOTE: one could pass some internal error code instead of messages
+that is more efficient but is coupling us with the logic and 
+implementation of making the error message
+*/
 
-// bellow we will be deliberately meandering between various
+////////////////////////////////////////////////////////////////
+
+// here we are emulating some busines logic  API
+// using the valstat protocol
+// we will be meandering between various
 // types when composing various adhoc valstat's
-// this is to show its maleability
-// and composability
+//
+// Values are inside or not inside the two valstat fields
+// For step one of decoding the valstat returned
+// the actual value is irelevant
+// only its field state of occupancy matters
 
-struct UserId final { };
+// vscall actually returns new valstat on error state
+// valstat error state is: "value field is empty"
+#define return_on_empty_value(VS) \
+    if (!VS.value)                \
+        return { {}, VS.status }
+
+#define vscall(VS, F) \
+    auto VS = F;      \
+    return_on_empty_value(VS)
+
+////////////////////////////////////////////////////////////
+// we emulate non-deterministic business system and logic
+// some data may be there or not there
+// random 0/1 flip-flop
+// srand() is called above
+#define RANDOM_0_OR_1 bool(rand() % 2)
+
+struct UserId final
+{
+};
 struct ContactsServer final
 {
     valstat<UserId> GetUserId()
     {
         static UserId uid_;
-        // random flip OK or ERROR metestate return
+        // random flip OK or ERROR metastate return
         if (RANDOM_0_OR_1)
+            // OK = occupied value field AND empty status field
             return {uid_, {}};
-        return {{}, errc_str(EFAULT, "while trying to obtain user id.")};
+
+        // ERROR = empty value field AND occupied status field
+        // error codes are random and arbitrary
+        return {{}, dbj_strerror(EFAULT, RED_MSG("while trying to obtain user id.") ) };
     }
 };
 
 struct Location final
 {
-    // we are deliberately meandering between various
-    // types when composing the various valstat's
-    // this is to show its maleability
-    // and composability
-    // remember we have choosen valstat to be struct of two std options
-    // where second option (aka status) is predefined to hold string
-    valstat<const char *> GetCityName() { return {"Valhala", {}}; }
+    valstat<const char *> GetCityName()
+    {
+        // OK = occupied value field AND empty status field
+        return {"Valhala", {}};
+    }
 };
+
 struct GeoServer final
 {
     valstat<Location> GetLocation(UserId *uid)
@@ -191,7 +143,8 @@ struct GeoServer final
         static Location loc_;
         if (RANDOM_0_OR_1)
             return {loc_, {}};
-        return {{}, errc_str(EBUSY, " upon getting the location. ")};
+        // ERROR = empty value field AND occupied status field
+        return {{}, dbj_strerror(EBUSY, RED_MSG("upon getting the location."))};
     }
 };
 
@@ -200,7 +153,8 @@ valstat<ContactsServer> GetOrOpenContactsServerConnection() noexcept
     static ContactsServer cs{};
     if (RANDOM_0_OR_1)
         return {cs, {}};
-    return {{}, errc_str(EINVAL, " while trying to get or open the server connection")};
+    // ERROR = empty value field AND occupied status field
+    return {{}, dbj_strerror(EINVAL, RED_MSG("while trying to get or open the server connection"))};
 }
 
 valstat<GeoServer> GetOrOpenGeoServerConnection() noexcept
@@ -208,71 +162,79 @@ valstat<GeoServer> GetOrOpenGeoServerConnection() noexcept
     static GeoServer gs_;
     if (RANDOM_0_OR_1)
         return {gs_, {}};
-    return {{}, errc_str(ESPIPE, "Could not open geo server connection")};
+    // ERROR = empty value field AND occupied status field
+    return {{}, dbj_strerror(ESPIPE, RED_MSG("Could not open geo server connection"))};
 }
+
 ///////////////////////////////////////////////////////////////////
-// call actually returns on error
-// and error is "value field is empty"
-// note the absolute key and core of the trick here
-// we do not look into the value, thus we do not need
-// to define what does it mean "empty" for each and every
-// type, value type might be
-// instead we just check if field potentially holding it is empty
-// notice we can return as we do because we know valstat is return typee
-// and valstat is an record of two fields
-#define return_on_no_value(VS) \
-    if (!VS.value)             \
-        return { {}, VS.status }
-#define call(VS, F) \
-    auto VS = F;    \
-    return_on_no_value(VS)
-
-// return type is record of two fields aka valstat
-inline valstat< const char * > FindUsersCity() noexcept
+// return type is record of two fields aka valstat<T>
+// vscall macro only checks the occupancy of the value field
+// T is irrelevant for the step one of valstat decoding
+inline valstat<const char *> FindUsersCity() noexcept
 {
-    call(contacts, GetOrOpenContactsServerConnection());
-    call(uid, contacts.value->GetUserId());
-    call(geo, GetOrOpenGeoServerConnection());
-    call(uloc, geo.value->GetLocation(&*uid.value));
-    call(cityname, (uloc.value)->GetCityName());
-    // all the call's above do return_on_no_value
-    // thus the value must exist here
-    return { *cityname.value, {} };
+    vscall(contacts, GetOrOpenContactsServerConnection());
+    vscall(uid, contacts.value->GetUserId());
+    vscall(geo, GetOrOpenGeoServerConnection());
+    vscall(uloc, geo.value->GetLocation(&*uid.value));
+    vscall(cityname, (uloc.value)->GetCityName());
+    // all the vscall's above do return_on_empty_value
+    // thus the value field must be "occupied" here
+    return {*cityname.value, {}};
 }
 
-#undef call
-#undef return_on_no_value
+#undef vscall
+#undef return_on_empty_value
 
 static int test_()
 {
     int id_ = 0;
-    // loop until city is found
+    // business logic: loop until city is found
     do
     {
         id_ += 1;
-        auto [city_name, errc] = FindUsersCity();
+        auto [city_name, status] = FindUsersCity();
 
+        // notice the two step decoding
+        // step one: is field "occupied"
+        // we do not need the type information
+        // of the value inside the field
         if (city_name)
         {
-            printf("\n%s", *city_name );
+            /*
+        step two: use the value field is holding
+        FindUsersCity() returns valstat<const char*>
+        thus city_name type is std::optional<const char *>
+        */
+            printf("\n\n" WHT_MSG("City name is: %s") "\n", *city_name);
             break;
         }
-
-        if (errc)
-            printf("\n%s", errc->data());
+        // if present the status means error
+        // step one: is field "occupied"
+        if (status)
+            // step two: use the value field is holding
+            printf("\n" RED_MSG("ERROR: ") "%s ", status->data);
 
     } while (true);
-return id_;
+    return id_;
 }
 
 int main()
 {
-
-    for (unsigned k = 0; k < 10; ++k)
-    {
-        // test "tries" until city is found
-        int tries = test_();
-        printf("\nFound after %4d tries", tries);
-        printf("\n---------------------------------");
-    }
+    // test "tries" until city is found
+    int tries = test_();
+    printf("\n" WHT_MSG("Found after %4d tries"), tries - 1);
+    printf("\n" WHT_MSG("---------------------------------"));
 }
+
+/*
+    VALSTAT for above but inside tight runtimes
+    depends on two facts:
+
+    if(!value) yields true for value == null
+    if(!status) yields true if status == 0
+	
+    template<typename T> struct valstat final {
+		T* value;
+ 		int status;
+	};
+*/
